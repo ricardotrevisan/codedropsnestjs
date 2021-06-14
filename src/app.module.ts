0b989b3445a7889ebea3cf3cca190c2ef44930af
Trevisan <ricardo.trevisan@icloud.com>
2021-06-14 18:01:30 -0300

    init

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,8 @@ import { UsersModule } from './users/users.module';
     UsersModule,
     MongooseModule.forRoot(
       //'mongodb://root:senha@localhost:27017/?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false',
-      'mongodb://root:senha@8.tcp.ngrok.io:12140/?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false',
+      //'mongodb://root:senha@8.tcp.ngrok.io:12140/?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false',
+      'mongodb+srv://trevisan:wqQkawTNusUY-Z9@cluster0.3vdsa.mongodb.net/codedrops?retryWrites=true&w=majority',
     ),
   ],
   controllers: [],
